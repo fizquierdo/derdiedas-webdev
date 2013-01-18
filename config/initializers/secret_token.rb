f5b7cef983266d48289cb9b19132f1a323fede98
Fernando Izquierdo <fer.izquierdo@gmail.com>
2013-01-18 23:59:18 +0100

    initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Derdiedas::Application.config.secret_token = '085441576ad4931ade9fbe4b95d75ce9156b96aa2c18022f87f3a357a15b1ea2b830b91321f338483ea68c74fc6f689d71ee6820f25a160c5129eec53c1da3b3'
