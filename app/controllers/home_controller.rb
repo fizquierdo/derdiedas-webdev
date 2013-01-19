e76c3484eb96d9df4b90ac76aa932e9936e79bd3
Fernando Izquierdo <fer.izquierdo@gmail.com>
2013-01-19 02:05:38 +0100

    changed notice

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,9 @@ class HomeController < ApplicationController
   def check
     @word = Word.find(params[:id])
     if params["chose_#{@word.article}"]
-      redirect_to root_url, :flash => { :success=> "OK,  #{@word.to_s} war richtig"}
+      redirect_to root_url, :flash => { :success=> "Sehr gut!,  #{@word.to_s} war richtig"}
     else
-      redirect_to root_url, :flash => { :error=> "Neeein,  #{@word.to_s} war richtig"}
+      redirect_to root_url, :flash => { :error=> "mmmmmm Eigentlich war #{@word.to_s} richtig"}
     end
   end
 end
