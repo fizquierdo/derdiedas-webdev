d7454d1cec4ad348eab3b679e1a1db424e98a15f
Fernando Izquierdo <fer.izquierdo@gmail.com>
2013-01-20 23:38:10 +0100

    remove invalid test

--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -1,11 +1,4 @@
 require 'spec_helper'
 
 describe "Home" do
-  let(:word) {FactoryGirl.create(:word)}
-  subject {page}
-  before{visit root_path}
-  #it "should have the content 'richtig'" do
-  #  # TODO pass this test
-  #  should have_content('richtig')
-  #end
 end
