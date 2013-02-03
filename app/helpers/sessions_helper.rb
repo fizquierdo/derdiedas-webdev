c4fc6f665ab27675e1e772a37dd6f802768b42b2
Fernando Izquierdo <fer.izquierdo@gmail.com>
2013-02-03 01:07:46 +0100

    up to friendly forwarding

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,9 @@ module SessionsHelper
   def current_user
     @current_user ||= User.find_by_remember_token(cookies[:remember_token]) 
   end
+  def current_user?(user)
+    current_user == user
+  end
   def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
