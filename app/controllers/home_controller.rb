1bab3492783c53703ba35ad2140ab4277d5fcca0
Fernando Izquierdo <fer.izquierdo@gmail.com>
2013-02-03 17:49:50 +0100

    create a guess when user clicks button

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,26 @@ class HomeController < ApplicationController
   end
   def check
     @word = Word.find(params[:id])
-    if params["chose_#{@word.article}"]
-      redirect_to root_url, :flash => { :success=> "Sehr gut!,  #{@word.to_s} war richtig"}
+    if given_answer == @word.article
+      flash_data = { :success=> "Sehr gut!,  #{@word.to_s} war richtig"}
     else
-      redirect_to root_url, :flash => { :error=> "mmmmmm Eigentlich war #{@word.to_error_s} richtig"}
+      flash_data = { :error=> "mmmmmm Eigentlich war #{@word.to_error_s} richtig"}
     end
+    save_guess if signed_in?
+    redirect_to root_url, :flash => flash_data
   end
+  private
+
+    def given_answer
+      answer = nil
+      %w(der die das).each do |article|
+        answer = article if params["chose_#{article}"]
+      end
+      answer
+    end
+    def save_guess
+      guess = current_user.guesses.build(word_id: @word.id, answer: given_answer)
+      guess.save
+    end
+
 end
--- spec/requests/homes_spec.rb
+++ spec/requests/homes_spec.rb
@@ -14,6 +14,7 @@ describe "Home" do
     before { click_button 'Der' }
     it { should have_selector('div.alert.alert-success', text: 'richtig') }
   end
+
   let(:wrong_word){"Eigentlich war der Baum richtig"}
   describe "user clicks wrong article" do
     before { click_button 'Das' }
@@ -24,5 +25,34 @@ describe "Home" do
     it { should have_error_message(wrong_word)}
   end
 
+  describe " no guess is created unless the user is logged in" do
+    it " should not increase guesses" do
+      expect{click_button 'Der'}.to change(Guess, :count).by(0)
+      expect{click_button 'Die'}.to change(Guess, :count).by(0)
+      expect{click_button 'Das'}.to change(Guess, :count).by(0)
+    end
+  end
+
+  describe " a guess is created if the user is logged in" do
+    let(:user) { FactoryGirl.create(:user)}
+    before do 
+      sign_in user
+      visit root_path
+    end
+    describe "user clicks wrong word" do
+      let(:clicked){"Das"}
+      it " should increase guesses" do
+        expect{click_button clicked}.to change(Guess, :count).by(1)
+        expect{click_button clicked}.to change(user.guesses, :count).by(1)
+      end
+    end
+    describe "user clicks right word" do
+      let(:clicked){"Der"}
+      it " should increase guesses" do
+        expect{click_button clicked}.to change(Guess, :count).by(1)
+        expect{click_button clicked}.to change(user.guesses, :count).by(1)
+      end
+    end
+  end
 
 end
