ab1ac053d1e6b6e22663665b2910d68e3e75b54a
Fernando Izquierdo-Carrasco <fer.izquierdo@gmail.com>
2013-06-28 16:02:58 +0200

    added uservoice js

---
+++ app/assets/javascripts/user_voice.js
@@ -0,0 +1,16 @@
+
+(function(){var uv=document.createElement('script');uv.type='text/javascript';uv.async=true;uv.src='//widget.uservoice.com/zI6t9nlUsdD2W68UfVufig.js';var s=document.getElementsByTagName('script')[0];s.parentNode.insertBefore(uv,s)})()
+
+
+UserVoice = window.UserVoice || [];
+UserVoice.push(['showTab', 'classic_widget', {
+    mode: 'full',
+    primary_color: '#cc6d00',
+    link_color: '#007dbf',
+    default_mode: 'support',
+    forum_id: 212689,
+    tab_label: 'Feedback & Support',
+    tab_color: '#cc6d00',
+    tab_position: 'middle-right',
+    tab_inverted: false
+}]);
